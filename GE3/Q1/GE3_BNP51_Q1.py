6e2443ddf0b889ed0363984811fa21e0ea4d220b
siatos <konstantinos.siatos@nokia.com>
2021-12-31 15:32:46 +0200

    error fix when adding edges

--- GE3/Q1/GE3_BNP51_Q1.py
+++ GE3/Q1/GE3_BNP51_Q1.py
@@ -1,11 +1,16 @@
+"""
+Script to solve Q1 for GE3
+Usage: python GE3_BNP51_Q1.py -m ppi_matrix.xlsx -n ppi_names.xlsx
+"""
+from constants import LOGFILE, SIZE_OF_NODESLIST
 from argparse import ArgumentParser
+import os
+import logging
 import networkx as nx
 import matplotlib.pyplot as plt
-import xlrd
+#import xlrd
 import pandas as pd
-import itertools
-import logging
-import os
+#import itertools
 
 def parse_input():
     """
@@ -20,84 +25,94 @@ def parse_input():
     return arguments
 
 def read_xcel_file(fname):
+    """
+    Read an excel file and return a list of values
+    The output of read is a pandas dataframe(df) that is converted into a list
+    """
     df = pd.read_excel(fname, index_col=None, header=None)
     #print(df)
     value_list = df.values.tolist()
     #value_dict = df.to_dict()
     return value_list
 
-def log2file(logline, loglevel):
+def log2file(log_line, loglevel):
+    """
+    Simple logging facility. Possible an overkill here.
+    """
     if loglevel.lower() == 'debug':
-       logging.debug(logline)
-    elif loglevel.lower() == 'error':  
-       logging.error(logline)
-    elif loglevel.lower() == 'info':  
-       logging.info(logline)
+        logging.debug(log_line)
+    elif loglevel.lower() == 'error':
+        logging.error(log_line)
+    elif loglevel.lower() == 'info':
+        logging.info(log_line)
     else:
-        logging.info('incoherent loglevel: ' + logline)
+        logging.info('incoherent loglevel: ' + log_line)
 
 
 def create_gene_dict(node_list, adj_matrix):
     """
-    Read data from a list of nodes and its adjacency matrix (represented here as a list of rows 
+    Read data from a list of nodes and its adjacency matrix (represented here as a list of rows
     and return a dictionary of nodes (each key is a node) and the number of nodes (a row having 1's and/or 0's )
     The dict should have a form like:
-      Gene_x:[0, 1, ..., 1] 300 elements in each list. There will be 300 keys (genes) in the dictionary
-    Essentialy this is the adjacency matrix of the graph  a 300x300 square matrix in a slightly different format 
+      Gene_x:[0, 1, ..., 1] 300 elements in each list. There will be 300 keys (genes) in the dictionary.
+      To create the dictionary iterate through the two lists  created form tables (matrix and names) at the same time.
+      There is no checking for incosistencies. Data are assumed to be correct.
+    Essentialy this is the adjacency matrix of the graph  a 300x300 square matrix in a slightly different format
     :param
          node_list: input node list
-         adj_matrix: input list of lists (rows) 
+         adj_matrix: input list of lists (rows)
     :return:
         return_dict: dictionary of nodes
         count: number of nodes
-    There are no validation/consistency checks on the input data    
+    There are no validation/consistency checks on the input data
     """
     # Initialize local values
     return_dict = {}
-    count = 0 
- 
-    # iterate through the lists simultaneously
+    count = 0
+
+    print("here")
+    print(node_list)
+
+    # iterate through the two lists simultaneously
     for (gene, row) in zip(node_list, adj_matrix):
-        key = "Gene_" + str(gene[0]) #each member of node_list is a list 
+        key = "Gene_" + str(gene[0]) #each member of node_list is a list
         #print(key)
-        return_dict[key] = row 
+        return_dict[key] = row
         count +=1
     return return_dict, count
 
 def get_nodes_by_degree(Grph, n):
     """
-    return a list of nodes depending on degree size in descending order 
+    return a list of nodes depending on degree size in descending order
     :param
-         Grph: graph 
-         n: size of list 
+         Grph: graph
+         n: size of list, i.e. return only the top n nodes
     :return:
-        degree_list: list of nodes sorted on their degree descending order 
+        degree_list: list of nodes sorted on their degree descending order
     """
     degree_list = list(Grph.degree())
     sorted_list = sorted(degree_list, key=take_second, reverse=True)
     #print("Degree for each node: {}".format(sorted_list))
-    print("=============================")
     print("The highest {} elements of list (in descending order) are : {}".format(n, str(sorted_list[:n])))
     return  sorted_list[:n]
 
 def get_betweenness_centrality(Grph, n):
     """
-    return a list of nodes depending on betweenes centrality in descending order 
+    return a list of nodes depending on betweenes centrality in descending order
     :param
-         Grph: graph 
-         n: size of list 
+         Grph: graph
+         n: size of list
     :return:
-        degree_list: list of nodes sorted on their betweeness centrality in descending order 
+        degree_list: list of nodes sorted on their betweeness centrality in descending order
     """
-    S = sorted(nx.betweenness_centrality(Grph).items(), key=lambda item: item[1], reverse=True)
-    print("=============================")
-    print("The highest {} elements of list (betweeness centrality in descending order) are : {}".format(n, str(S[:n])))
-    return S[:n]
+    SGrph = sorted(nx.betweenness_centrality(Grph).items(), key=lambda item: item[1], reverse=True)
+    print("The highest {} elements of list (betweeness centrality in descending order) are : {}".format(n, str(SGrph[:n])))
+    return SGrph[:n]
 
 def take_second(elem):
     """
     used to return the second element of an (element) tuple
-    i could not make lambdha expression work properly, sad ...
+    i could not make lambda expression work properly, sad ...
     """
     return elem[1]
 
@@ -110,7 +125,7 @@ if __name__ == '__main__':
         os.remove(LOGFILE)
     else:    # if there is no logfile show an error and continue ##
         print("Error: {} file not found, ...ok".format(LOGFILE))
-    
+
     logging.basicConfig(filename=LOGFILE, level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     logging.info('Started')
 
@@ -125,61 +140,68 @@ if __name__ == '__main__':
     gene_dict, no_of_nodes = create_gene_dict(nodes_list, adj_list)
     # print(gene_dict)
 
-    # create a undirected graph based on above info. 
+    # create a undirected graph based on above info.
     G=nx.Graph()
 
-    # add the nodes
+    # add the nodes, all nodes will be in the form Gene_n, where n some integer
     for node in gene_dict:
-        #print("Adding Node: {}".format(node))
+        print("Adding Node: {}".format(node))
         G.add_node(str(node))
 
     print("graph created with {} nodes".format(G.number_of_nodes()))
 
     # add the edges
     for node in gene_dict:
-        #print("Node is: {}: {}".format(node, gene_dict[node]))
-        for i, val in enumerate(gene_dict[node]):
-        #print("pos {} value {}".format(i+1, val)) 
-            if int(val):
-                end="Gene_"+str(i + 1)
-                #print("Adding EDGE: {} {}".format(node,end))
-                G.add_edge(node, end)
+       # for each node create a list with the pos of nonzero elements of each row
+       nonzero_vals = [i for i,x in enumerate(gene_dict[node]) if x == 1]
+       end_nodes = []
+       #find end node from pos in the list
+       for pos in nonzero_vals:
+           end_node = 'Gene_'+ str(nodes_list[pos][0])
+           #print(end_node)
+           end_nodes.append(end_node)
+           #print("{} {}".format(nodes_list[pos][0], type(nodes_list[pos][0])))
+           #G.add_edge(node, end_node)
+       print("found for node {} following nodes {}".format(node, end_nodes))
+       for endnode in end_nodes:
+           #we re usning undirected graphs so direction is not important
+           if G.has_edge(node, endnode) or G.has_edge(endnode, node):
+               print("Edge from {} to {} exists no adding".format(node, endnode))
+           else:
+               print("Adding Edge from {} to {} ".format(node, endnode))
+               G.add_edge(node, endnode)
+
+
 
     nx.draw_circular(G, with_labels=True)
     #nx.draw(G)
 
-
-    print("Find Degree for each node ... show the highest 5 nodes")
-    get_nodes_by_degree(G, 10)
-
-    print("Find betweeness centrality ...")
-    get_betweenness_centrality(G, 10)
-
-    #print("Find Degree for each node ...")
-    #degree_list = list(G.degree())
-    #sorted_list = sorted(degree_list, key=take_second)
-    ##print("Degree {}".format(sorted_list))
-    #print("============================")
-    #print("The highest 5 elements of list (in ascending order) are : " + str(sorted_list[-5:]))
+    print("============================ HIGHEST DEGREE NODES ===============================")
+    print("Find Degree for each node ... show the highest {} nodes".format(SIZE_OF_NODESLIST))
+    get_nodes_by_degree(G, SIZE_OF_NODESLIST)
+    print("=================================================================================")
 
 
-    #largest_cc = max(nx.connected_components(G), key=len)
-    #S = G.subgraph(largest_cc).copy()
-    #print("Number of nodes for connected subgraph component S: {}".format(S.number_of_nodes()))
-    #print(S)
+    print("======================== HIGHEST BETWENESS CENTRALITY NODES =====================")
+    print("Find betweeness centrality ...show the highest {} nodes".format(SIZE_OF_NODESLIST))
+    get_betweenness_centrality(G, SIZE_OF_NODESLIST)
+    print("==================================================================================")
+
 
     log2file('Logging all nodes for each connected component', 'info')
-    
-    for c in sorted(nx.connected_components(G), key=len, reverse=True): 
+
+    connected_components_count = 0
+    for c in sorted(nx.connected_components(G), key=len, reverse=True):
         S = G.subgraph(c).copy()
-        logline = print(list(S.nodes()))
-        log2file(logline, 'info')
-        logline = print("Number of nodes for connected subgraph component S: {}".format(S.number_of_nodes()))
+        connected_components_count += 1
+        logline = "for subgraph S with {} nodes: {}".format(S.number_of_nodes(), list(S.nodes()))
         log2file(logline, 'info')
 
+    print("============================ CONNECTED COMPONENTS ================================")
+    print("Found {} connected components ".format(connected_components_count))
+    print("==================================================================================")
 
     print("Starting to plot ...")
     plt.axis("off")
     plt.show()
     logging.info('Finished')
-
